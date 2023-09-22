1fd7b29c3ae573daefa8a2fe57c51a228e888887
Denis Manoevski <denis28381@gmail.com>
2023-09-22 12:41:01 +0300

    Started Conditional Statements

---
+++ Module_0_CSharp_Basics/Conditional Statements - Exercise/01. Sum Seconds/01. Sum Seconds.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace _01._Sum_Seconds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+
+            int total = a + b + c;
+
+            int min = total / 60;
+            int sec = total % 60;
+
+            if (sec < 10)
+            {
+                Console.WriteLine($"{min}:0{sec}");
+            }
+
+            else
+                Console.WriteLine($"{min}:{sec}");
+
+        }
+    }
+}
