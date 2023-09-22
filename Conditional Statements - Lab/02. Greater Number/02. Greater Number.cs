b7debde51c50e6c6f5638148e2aa0771a253bae7
Denis Manoevski <denis28381@gmail.com>
2023-09-22 12:06:58 +0300

    Started Conditional Statements - Lab
    Added 01. Excellent Result and 02. Greater Number

---
+++ Conditional Statements - Lab/02. Greater Number/02. Greater Number.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace _02._Greater_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+
+            if (a > b)
+            {
+                Console.WriteLine(a);
+            }
+            else
+            {
+                Console.WriteLine(b);
+            }
+        }
+    }
+}
