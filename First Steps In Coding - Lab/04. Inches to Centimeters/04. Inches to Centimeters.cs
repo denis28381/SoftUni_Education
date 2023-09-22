d6e5f9c2850fc5b380c61537b5eefa514dfec686
Denis Manoevski <denis28381@gmail.com>
2023-09-22 11:46:03 +0300

    Finished all

---
+++ First Steps In Coding - Lab/04. Inches to Centimeters/04. Inches to Centimeters.cs
@@ -0,0 +1,14 @@
+﻿namespace _04._Inches_to_Centimeters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double i = double.Parse(Console.ReadLine());
+            double itocm = 2.54;
+            double cm = (i * itocm);
+
+            Console.WriteLine(cm);
+        }
+    }
+}
---
+++ First Steps In Coding - Lab/05. Greeting by Name/05. Greeting by Name.cs
@@ -0,0 +1,10 @@
+﻿namespace _05._Greeting_by_Name
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
