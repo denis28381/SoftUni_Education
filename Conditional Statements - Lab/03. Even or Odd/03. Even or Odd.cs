a162b97bce2e2ab1ed24f26e26374cea805689be
Denis Manoevski <denis28381@gmail.com>
2023-09-22 12:13:58 +0300

    Finished all exercises

---
+++ Conditional Statements - Lab/03. Even or Odd/03. Even or Odd.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace _03._Even_or_Odd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+
+            if (a % 2 == 0)
+            {
+                Console.WriteLine("even");
+            }
+            else
+            {
+                Console.WriteLine("odd");
+            }
+        }
+    }
+}
---
+++ Conditional Statements - Lab/07. Area of Figures/07. Area of Figures.cs
@@ -0,0 +1,62 @@
+﻿using System;
+namespace _07._Area_of_Figures
+{
+    class Program
+    {
+        public void square()
+        {
+            double a = double.Parse(Console.ReadLine());
+            a *= a;
+            Console.WriteLine(a);
+        }
+
+        public void rectangle()
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double c = a * b;
+            Console.WriteLine(c);
+        }
+
+        public void circle()
+        {
+            double a = double.Parse(Console.ReadLine());
+            double c = Math.PI * a * a;
+            Console.WriteLine(c);
+        }
+
+        public void triangle()
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double c = (a * b) / 2;
+            Console.WriteLine(c);
+
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            string type = Console.ReadLine();
+
+            if (type == "square")
+            {
+                p.square();
+            }
+
+            if (type == "rectangle")
+            {
+                p.rectangle();
+            }
+
+            if (type == "circle")
+            {
+                p.circle();
+            }
+
+            if (type == "triangle")
+            {
+                p.triangle();
+            }
+        }
+    }
+}
