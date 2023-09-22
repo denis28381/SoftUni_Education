dd034bcafd9382e996129b51bfdca2832510365c
Denis Manoevski <denis28381@gmail.com>
2023-09-22 12:45:40 +0300

    Finished ex 3

---
+++ Module_0_CSharp_Basics/Conditional Statements - Exercise/03. Time + 15 Minutes/03. Time + 15 Minutes.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace _03._Time___15_Minutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int h = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+
+            m += 15;
+
+            if (m > 59)
+            {
+                h++;
+                m -= 60;
+            }
+
+            if (h >= 24)
+            {
+                h -= 24;
+            }
+
+            if (m < 10)
+            {
+                Console.WriteLine(h + ":0" + m);
+            }
+            else
+                Console.WriteLine(h + ":" + m);
+        }
+    }
+}
