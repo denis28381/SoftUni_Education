917351350b0567da8f866d1ef1eca7e3c4a11e18
Denis Manoevski <denis28381@gmail.com>
2023-09-22 16:58:06 +0300

    Added and finished Nested Loops - Lab

---
+++ Module_0_CSharp_Basics/Nested Loops - Lab/04. Sum of Two Numbers/04. Sum of Two Numbers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace _04._Sum_of_Two_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int res = int.Parse(Console.ReadLine());
+
+            int count = 0;
+
+            for (int a = num1; a <= num2; a++)
+            {
+                for (int b = num1; b <= num2; b++)
+                {
+                    count++;
+                    if (a + b == res)
+                    {
+                        Console.WriteLine($"Combination N:{count} ({a} + {b} = {res})");
+                        return;
+                    }
+                }
+            }
+            Console.WriteLine($"{count} combinations - neither equals {res}");
+        }
+    }
+}
---
+++ Module_0_CSharp_Basics/Nested Loops - Lab/06. Building/06. Building.cs
@@ -0,0 +1,39 @@
+﻿namespace _06._Building
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int floors = int.Parse(Console.ReadLine());
+            int rooms = int.Parse(Console.ReadLine());
+
+            int f = floors;
+            for (int a = 1; f >= a; f--)
+            {
+                for (int b = 0; b < rooms; b++)
+                {
+
+                    if (floors == f)
+                    {
+                        Console.Write($"L{f}{b} ");
+                    }
+                    else if (f % 2 == 0)
+                    {
+                        Console.Write($"O{f}{b} ");
+                    }
+                    else
+                    {
+                        Console.Write($"A{f}{b} ");
+                    }
+
+                    if (b == rooms - 1)
+                    {
+                        Console.Write("\n");
+                    }
+                }
+            }
+
+        }
+    }
+}
