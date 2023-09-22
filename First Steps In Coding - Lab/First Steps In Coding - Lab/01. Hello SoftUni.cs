b84ec36c05970c3ee2630ce075feb20f363ea7f5
Denis Manoevski <denis28381@gmail.com>
2023-09-22 11:34:02 +0300

    Created 2 new projects

---
+++ First Steps In Coding - Lab/First Steps In Coding - Lab/01. Hello SoftUni.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace First_Steps_In_Coding___Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello SoftUni");
+        }
+    }
+}
