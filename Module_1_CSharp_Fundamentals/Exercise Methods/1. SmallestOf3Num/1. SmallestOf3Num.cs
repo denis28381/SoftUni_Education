b3dcf96a3d3578e8825f4bd6a7ee7ff41912ac82
Denis Manoevski <denis28381@gmail.com>
2023-10-09 20:36:01 +0300

    ---

---
+++ Module_1_CSharp_Fundamentals/Exercise Methods/1. SmallestOf3Num/1. SmallestOf3Num.cs
@@ -0,0 +1,38 @@
+﻿namespace _1._SmallestOf3Num
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            GetSmallest();
+
+        }
+
+        static void GetSmallest()
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+
+            int smallest = Int32.MaxValue;
+
+            if (a < smallest)
+            {
+                smallest = a;
+            }
+
+            if (b < smallest)
+            {
+                smallest = b;
+            }
+
+            if (c < smallest)
+            {
+                smallest = c;
+            }
+
+            Console.WriteLine(smallest);
+        }
+    }
+}
---
+++ Module_1_CSharp_Fundamentals/Exercise Methods/2. Vowels Count/2. Vowels Count.cs
@@ -0,0 +1,28 @@
+﻿namespace _2._Vowels_Count
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //a e i o u
+            GetVowelsCount();
+
+        }
+
+        static void GetVowelsCount()
+        {
+            int VowelsCount = 0;
+            string input = Console.ReadLine();
+            for (int j = 0; j < input.Length; j++)
+            {
+                char currennt = input[j];
+                if (currennt == 'a'|| currennt == 'e'|| currennt == 'i' || currennt == 'o' || currennt == 'u')
+                {
+                    VowelsCount++;
+                }
+            }
+            Console.WriteLine(VowelsCount);
+        }
+
+    }
+}
