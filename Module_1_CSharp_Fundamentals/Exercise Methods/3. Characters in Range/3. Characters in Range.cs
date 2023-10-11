e4c41ad0114f4027e28235b4f6abeeac719e6b58
Denis Manoevski <denis28381@gmail.com>
2023-10-11 23:53:46 +0300

    ----

---
+++ Module_1_CSharp_Fundamentals/Exercise Methods/3. Characters in Range/3. Characters in Range.cs
@@ -0,0 +1,35 @@
+﻿using System.Formats.Asn1;
+
+namespace _3._Characters_in_Range
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GetCharRange();
+        }
+
+        static void GetCharRange()
+        {
+            char a = char.Parse(Console.ReadLine());
+            char b = char.Parse(Console.ReadLine());
+
+            bool cont = true;
+            while (cont)
+            {
+                if (a != b -1)
+                {
+                    a++;
+                    Console.Write(a + " ");
+                    
+                }
+                else
+                {
+                    cont = false;
+                }
+            }
+            
+
+        }
+    }
+}
---
+++ Module_1_CSharp_Fundamentals/Exercise Methods/4. Password Validator/4. Password Validator.cs
@@ -0,0 +1,65 @@
+﻿using System.Threading.Channels;
+
+namespace _4._Password_Validator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pass = Console.ReadLine();
+            
+
+            if (checkLength(pass) == true && checkLettersDigits(pass) == true && checkTwoDigits(pass) == true)
+            {
+                Console.WriteLine("Password is valid");
+            }
+
+        }
+
+        static bool checkLength(string password)
+        {
+            if (password.Length <= 6 && password.Length >= 10)
+            {
+                Console.WriteLine("Password must be between 6 and 10 characters ");
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        static bool checkLettersDigits(string password)
+        {
+            foreach (char c in password)
+            {
+                if (!char.IsLetterOrDigit(c))
+                {
+                    Console.WriteLine("Password must consist only of letters and digits");
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        static bool checkTwoDigits(string password)
+        {
+            int digitCount = 0;
+
+            foreach (char c in password)
+            {
+                if (char.IsDigit(c))
+                {
+                    digitCount++;
+                    if (digitCount >= 2)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            Console.WriteLine("Password must have at least 2 digits");
+            return false;
+        }
+    }
+}
